7b4eb6132e12f541413b2558292e5906d65fe0a9
Nikolay Adamov <nikolay.adamov@servolux.by>
2026-07-10 12:53:43 +0300

    Add creator handler

--- app/web/routers/order.py
+++ app/web/routers/order.py
@@ -1,6 +1,7 @@
 import logging
 
 from fastapi import APIRouter, HTTPException, status
+from pydantic import BaseModel
 from src.services.order import Order, OrderAdd, Orders
 
 from ..dependencies import DBSessionDep, UserDep
@@ -23,3 +24,22 @@ async def make_order(user_session: UserDep, order: OrderAdd, db_session: DBSessi
 async def get_order(user_session: UserDep, order_id: int, db_session: DBSessionDep):
     orders = Orders(db_session)
     return await orders.get(order_id)
+
+
+
+class Creator(BaseModel):
+    id: int
+    name: str
+
+
+@router.get("/creators", response_model=list[Creator])
+async def get_creators(user_session: UserDep, db_session: DBSessionDep):
+    return list[
+        Creator(id=1, name="User #1"),
+        Creator(id=2, name="User #2"),
+        Creator(id=3, name="User #3"),
+        Creator(id=4, name="User #4"),
+        Creator(id=5, name="User #5"),
+    ]
+    # orders = Orders(db_session)
+    # return await orders.get(order_id)
